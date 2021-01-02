8cf850cb779ad7a620c0b02e5f24ead8d54af524
Сергей <sergolongday@mail.ru>
2021-01-02 21:15:39 +0300

    added a few classes

--- HelloSkillbox.cpp
+++ HelloSkillbox.cpp
@@ -1,34 +1,64 @@
 ﻿#include <iostream>
-#define CURRENT_DATE 2
+#include <math.h>
+
+class Auto {
+public:
+    Auto() : ModelAuto("None"), costAuto(0) {}
+
+    Auto(std::string ModelAuto, int costAuto) {
+        this->ModelAuto = ModelAuto;
+        this->costAuto = costAuto;
+    }
+
+    void printInfoAboutAuto() {
+        std::cout << "Model : " << ModelAuto << std::endl;
+        std::cout << "Cost auto: " << costAuto << std::endl;
+    }
+
+    std::string getModelAuto() { return ModelAuto; }
+    int getCostAuto() { return costAuto; }
+
+private:
+    std::string ModelAuto;
+    int costAuto;
+};
+
+class Vector {
+public:
+    Vector() : x(0), y(0), z(0), moduleVector(0)
+    {}
+
+    Vector(double x, double y, double z) {
+        this->x = x;
+        this->y = y;
+        this->z = z;
+        moduleVector = sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+    }
+
+    void PrintVector() {
+        std::cout << '\n' << x << " " << y << " " << z << std::endl;
+    }
+
+    double GetModuleVector() { return moduleVector; }
+
+private:
+    double x;
+    double y;
+    double z;
+    double moduleVector;
+};
+
 
 int main() {
-    const int N = 5;
-    int arr[N][N];
-    int sum = 0;
-    
+    Auto car("BMW", 10000);
+    Vector vec(1,2,3);
 
-    // заполнение матрицы
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            arr[i][j] = i + j;
-        }
-    }
+    std::cout << "Ex1 - show info about car\n";
+    car.printInfoAboutAuto();
 
-    // вывод матрицы
-    std::cout << "Matrix: \n";
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            std::cout << arr[i][j] << '\t';
-        }
-        std::cout << std::endl;
-    }
     std::cout << std::endl;
-
-    // суммирование строки, индекс которой определяется остатком от деления текущей даты календаря
-    for (int i = 0; i < N; i++) {
-        sum += arr[CURRENT_DATE % N][i];
-    }
-    std::cout << "Result sum row " << CURRENT_DATE % N << " = " << sum << std::endl;
+    std::cout << "Ex2 - get module vector\n";
+    std::cout << "Result module vector: " << vec.GetModuleVector() << std::endl;
 
     return 0;
 }
