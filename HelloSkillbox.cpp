c8ca74ab08fef2c2f06467a867c1f13feed32df2
Сергей <sergolongday@mail.ru>
2021-01-03 22:52:24 +0300

    edit for new arr

--- HelloSkillbox.cpp
+++ HelloSkillbox.cpp
@@ -1,27 +1,45 @@
 ﻿#include <iostream>
-#include <vector>
 
 class Stack {
 public:
     int pop() {
         int tempValue;
-        tempValue = arr[arr.size() - 1];
-        arr.pop_back();
+        int* newArray = new int[size - 1];
+        tempValue = arr[0];
+        
+        for (int i = 1; i < size; i++) {
+            newArray[i - 1] = arr[i];
+        }
+
+        size--;
+        delete[] arr;
+        arr = newArray;
         return tempValue;
     }
-    void push( int pushValue) {
-        arr.insert(arr.begin(), pushValue);
+    void push(int pushValue) {
+        int* newArray = new int[size + 1];
+
+        newArray[0] = pushValue;
+        for (int i = 0; i < size; i++)
+        {
+            newArray[i + 1] = arr[i];
+        }
+
+        size++;
+        delete[] arr;
+        arr = newArray;
     }
 
     void printStack() {
-        for (int i = 0; i < arr.size(); i++) {
+        for (int i = 0; i < size; i++) {
             std::cout << arr[i] << " ";
         }
         std::cout << std::endl;
     }
 
 private:
-    std::vector<int> arr;
+    int* arr;
+    int size = 0;
 };
 
 int main() {
@@ -36,6 +54,9 @@ int main() {
 
     std::cout << "pop value from stack\n";
     std::cout << "Value is " << stack.pop() << std::endl;
+    std::cout << "Our stack\n";
+    stack.printStack();
 
+  
     return 0;
 }
