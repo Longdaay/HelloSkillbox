bb01ef6dcce13055495c3d80c227beb460077cc3
Сергей <sergolongday@mail.ru>
2021-01-03 23:29:01 +0300

    inherit classes

--- HelloSkillbox.cpp
+++ HelloSkillbox.cpp
@@ -1,62 +1,48 @@
 ﻿#include <iostream>
-
-class Stack {
+#include <vector>
+class Animal {
 public:
-    int pop() {
-        int tempValue;
-        int* newArray = new int[size - 1];
-        tempValue = arr[0];
-        
-        for (int i = 1; i < size; i++) {
-            newArray[i - 1] = arr[i];
-        }
+	virtual void Voice() {
+		std::cout << "Voice\n";
+	}
+};
 
-        size--;
-        delete[] arr;
-        arr = newArray;
-        return tempValue;
-    }
-    void push(int pushValue) {
-        int* newArray = new int[size + 1];
+class Dog : public Animal {
+public:
+	void Voice() override {
+		std::cout << "Woof!\n";
+	}
+};
 
-        newArray[0] = pushValue;
-        for (int i = 0; i < size; i++)
-        {
-            newArray[i + 1] = arr[i];
-        }
+class Cat : public Animal {
+public:
+	void Voice() override {
+		std::cout << "Meow!\n";
+	}
+};
 
-        size++;
-        delete[] arr;
-        arr = newArray;
-    }
+class Cow : public Animal {
+public:
 
-    void printStack() {
-        for (int i = 0; i < size; i++) {
-            std::cout << arr[i] << " ";
-        }
-        std::cout << std::endl;
-    }
-
-private:
-    int* arr;
-    int size = 0;
+	void Voice() override {
+		std::cout << "Moo!\n";
+	}
 };
 
 int main() {
-    Stack stack;
-    
-    for (int i = 0; i < 10; i++) {
-        std::cout << "Push in stack value: " << i << std::endl;
-        stack.push(i);
-    }
-    std::cout << "Our stack\n";
-    stack.printStack();
+	int size = 3;
+	Animal** animal = new Animal * [size];
+	Animal* cow = new Cow;
+	Animal* dog = new Dog;
+	Animal* cat = new Cat;
 
-    std::cout << "pop value from stack\n";
-    std::cout << "Value is " << stack.pop() << std::endl;
-    std::cout << "Our stack\n";
-    stack.printStack();
+	animal[0] = cow;
+	animal[1] = dog;
+	animal[2] = cat;
 
-  
-    return 0;
+	for (int i = 0; i < size; i++) {
+		animal[i]->Voice();
+	}
+
+	return 0;
 }
