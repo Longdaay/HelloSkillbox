d974410a505f1d385eb2fc70b9dfc769d33a9b19
Сергей <sergolongday@mail.ru>
2021-01-03 15:58:09 +0300

    homework 18 - class stack

--- HelloSkillbox.cpp
+++ HelloSkillbox.cpp
@@ -1,64 +1,41 @@
 ﻿#include <iostream>
-#include <math.h>
+#include <vector>
 
-class Auto {
+class Stack {
 public:
-    Auto() : ModelAuto("None"), costAuto(0) {}
-
-    Auto(std::string ModelAuto, int costAuto) {
-        this->ModelAuto = ModelAuto;
-        this->costAuto = costAuto;
+    int pop() {
+        int tempValue;
+        tempValue = arr[arr.size() - 1];
+        arr.pop_back();
+        return tempValue;
+    }
+    void push( int pushValue) {
+        arr.insert(arr.begin(), pushValue);
     }
 
-    void printInfoAboutAuto() {
-        std::cout << "Model : " << ModelAuto << std::endl;
-        std::cout << "Cost auto: " << costAuto << std::endl;
+    void printStack() {
+        for (int i = 0; i < arr.size(); i++) {
+            std::cout << arr[i] << " ";
+        }
+        std::cout << std::endl;
     }
-
-    std::string getModelAuto() { return ModelAuto; }
-    int getCostAuto() { return costAuto; }
 
 private:
-    std::string ModelAuto;
-    int costAuto;
+    std::vector<int> arr;
 };
-
-class Vector {
-public:
-    Vector() : x(0), y(0), z(0), moduleVector(0)
-    {}
-
-    Vector(double x, double y, double z) {
-        this->x = x;
-        this->y = y;
-        this->z = z;
-        moduleVector = sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
-    }
-
-    void PrintVector() {
-        std::cout << '\n' << x << " " << y << " " << z << std::endl;
-    }
-
-    double GetModuleVector() { return moduleVector; }
-
-private:
-    double x;
-    double y;
-    double z;
-    double moduleVector;
-};
-
 
 int main() {
-    Auto car("BMW", 10000);
-    Vector vec(1,2,3);
+    Stack stack;
+    
+    for (int i = 0; i < 10; i++) {
+        std::cout << "Push in stack value: " << i << std::endl;
+        stack.push(i);
+    }
+    std::cout << "Our stack\n";
+    stack.printStack();
 
-    std::cout << "Ex1 - show info about car\n";
-    car.printInfoAboutAuto();
-
-    std::cout << std::endl;
-    std::cout << "Ex2 - get module vector\n";
-    std::cout << "Result module vector: " << vec.GetModuleVector() << std::endl;
+    std::cout << "pop value from stack\n";
+    std::cout << "Value is " << stack.pop() << std::endl;
 
     return 0;
 }
